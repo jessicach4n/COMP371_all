bb0c9c53a6e0432eaf1983e07aa8af82649b606d
jessicach4n <j_chan20@live.concordia.ca>
2026-01-26 20:53:18 -0500

    Add build artifacts for CMake projects
    Initial addition of CMake-generated build files and binaries for COMP371_RaytracerBase and Lab_capsules capsules 1 and 2. These files are produced by running CMake and building the projects, and are required for compilation and execution.

---
+++ COMP371_RaytracerBase/code/build/CMakeFiles/raytracer.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/raytracer.dir/external/simpleppm.cpp.o"
+  "CMakeFiles/raytracer.dir/external/test_eigen.cpp.o"
+  "CMakeFiles/raytracer.dir/external/test_json.cpp.o"
+  "CMakeFiles/raytracer.dir/external/test_ppm.cpp.o"
+  "CMakeFiles/raytracer.dir/main.cpp.o"
+  "raytracer"
+  "raytracer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/raytracer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Lab_capsules/capsule1/code/build/CMakeFiles/capsule01.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/capsule01.dir/capsule01.cpp.o"
+  "capsule01"
+  "capsule01.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/capsule01.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
